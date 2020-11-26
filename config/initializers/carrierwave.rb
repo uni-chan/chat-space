930fc5b7c8a6caadac1ed7daac0bed8c929f1ff4
uni-chan <hkrm1107@gmail.com>
2020-11-26 19:02:30 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,22 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1'
-    }
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.storage = :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#       region: 'ap-northeast-1'
+#     }
   
-    config.fog_directory  = 'yamprocs6'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yamprocs6'
-  else
-    # 開発環境の時
-    config.storage = :file
-  end
-end
+#     config.fog_directory  = 'yamprocs6'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yamprocs6'
+#   else
+#     # 開発環境の時
+#     config.storage = :file
+#   end
+# end
